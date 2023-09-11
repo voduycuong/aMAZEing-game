f7e1d50ff2a651c1f8674abc53180dc7adcb4035
Anh <anhtm621@gmail.com>
2023-09-11 12:32:52 +0700

    fixed typo

--- src/kernel.c
+++ src/kernel.c
@@ -22,7 +22,7 @@ void main()
 
 void cli()
 {
-	drawStringARGB32(0, 100, "abcdefghijklmnopqrstuvwxyz", 0x00FF0000, 3);
+	drawStringARGB32(0, 100, "abcdefghijklmnopqrstuvwxyz", 0x00FFFFFF, 3);
 	drawStringARGB32(0, 150, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0x00AA0000, 3);
 	// drawCharARGB32('a', 100, 100, 0x00ffffff);
 
